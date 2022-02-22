bc462ecfdbcc9c64bbf722ebb3bce83f3063ea14
JosephineAkello <josephineakello2030@gmail.com>
2022-02-23 00:32:24 +0300

    add UI user input details

--- lib/main.dart
+++ lib/main.dart
@@ -18,28 +18,63 @@ class MyApp extends StatelessWidget {
   }
 }
 
-  class HomePage extends StatefulWidget{
-    const HomePage({Key? key}) : super(key: key);
-     
-     @override
-     HomePageState createState() => HomePageState();
-  }
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  class HomePageState extends State<HomePage> {
+  @override
+  HomePageState createState() => HomePageState();
+}
 
-    @override
-    Widget build(context){
-      return Scaffold(
-       appBar: AppBar(
-              title: const Text('Flutter + Flutterwave'),
-              centerTitle: true,
+class HomePageState extends State<HomePage> {
+  final TextEditingController fullname = TextEditingController();
+  final TextEditingController phone = TextEditingController();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController amount = TextEditingController();
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter + Flutterwave'),
+          centerTitle: true,
+        ),
+        body: Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+       child: Column(
+          children: [
+          const Padding(padding: EdgeInsets.all(10.0)),
+          Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            child: TextFormField(
+              controller: fullname,
+              decoration: const InputDecoration(labelText: "Full Name"),
             ),
-            body: Center(
-              child:ElevatedButton(
-                child: const Text('Pay with Flutterwave'),
-                onPressed: () {},
-              ),
-            )
-      );
-    }
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            child: TextFormField(
+              controller: phone,
+              decoration: const InputDecoration(labelText: "Phone Number"),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            child: TextFormField(
+              controller: email,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            child: TextFormField(
+              controller: amount,
+              decoration: const InputDecoration(labelText: "Amount"),
+            ),
+          ),
+          ElevatedButton(
+            child: const Text('Pay with Flutterwave'),
+            onPressed: () {},
+          ),
+        ])));
   }
+}
