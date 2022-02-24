f0989c3b4c3fbc8c4f0b4efdf45d5de771719726
JosephineAkello <josephineakello2030@gmail.com>
2022-02-24 21:11:04 +0300

    format document

--- lib/main.dart
+++ lib/main.dart
@@ -44,67 +44,72 @@ class HomePageState extends State<HomePage> {
           centerTitle: true,
         ),
         body: Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-       child: Form(
-            key: formKey,
-         child:Column(
-          children: [
-          const Padding(padding: EdgeInsets.all(10.0)),
-          Container(
-            margin: const EdgeInsets.only(bottom: 10),
-            child: TextFormField(
-              controller: fullname,
-              decoration: const InputDecoration(labelText: "Full Name"),
-                   validator: (value) =>
-                      value!.isNotEmpty? null : "Please fill in Your Name",
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 10),
-            child: TextFormField(
-              controller: phone,
-              decoration: const InputDecoration(labelText: "Phone Number"),
-               validator: (value) =>
-                      value!.isNotEmpty? null : "Please fill in Your Phone number",
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 10),
-            child: TextFormField(
-              controller: email,
-              decoration: const InputDecoration(labelText: "Email"),
-                validator: (value) =>
-                      value!.isNotEmpty? null : "Please fill in Your Email",
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 10),
-            child: TextFormField(
-              controller: amount,
-              decoration: const InputDecoration(labelText: "Amount"),
-              validator: (value) =>
-                      value!.isNotEmpty? null : "Please fill in the Amount you are Paying",
-                ),
-          ),
-          ElevatedButton(
-            child: const Text('Pay with Flutterwave'),
-            onPressed: () {
-              final name = fullname.text;
-              final userPhone = phone.text;
-              final userEmail = email.text;
-              final amountPaid = amount.text;
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Form(
+                key: formKey,
+                child: Column(children: [
+                  const Padding(padding: EdgeInsets.all(10.0)),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: TextFormField(
+                      controller: fullname,
+                      decoration: const InputDecoration(labelText: "Full Name"),
+                      validator: (value) =>
+                          value!.isNotEmpty ? null : "Please fill in Your Name",
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: TextFormField(
+                      controller: phone,
+                      decoration:
+                          const InputDecoration(labelText: "Phone Number"),
+                      validator: (value) => value!.isNotEmpty
+                          ? null
+                          : "Please fill in Your Phone number",
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: TextFormField(
+                      controller: email,
+                      decoration: const InputDecoration(labelText: "Email"),
+                      validator: (value) => value!.isNotEmpty
+                          ? null
+                          : "Please fill in Your Email",
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: TextFormField(
+                      controller: amount,
+                      decoration: const InputDecoration(labelText: "Amount"),
+                      validator: (value) => value!.isNotEmpty
+                          ? null
+                          : "Please fill in the Amount you are Paying",
+                    ),
+                  ),
+                  ElevatedButton(
+                    child: const Text('Pay with Flutterwave'),
+                    onPressed: () {
+                      final name = fullname.text;
+                      final userPhone = phone.text;
+                      final userEmail = email.text;
+                      final amountPaid = amount.text;
 
-              if (formKey.currentState!.validate()) {
-              _makeFlutterwavePayment(context,name,userPhone,userEmail,amountPaid);
-             }
-            },
-          ),
-        ]))));
+                      if (formKey.currentState!.validate()) {
+                        _makeFlutterwavePayment(
+                            context, name, userPhone, userEmail, amountPaid);
+                      }
+                    },
+                  ),
+                ]))));
   }
 
   //Add a method to make the flutter wave payment
   //This Method includes all the values needed to create the Flutterwave Instance
-void _makeFlutterwavePayment(BuildContext context, String fullname, String phone, String email, String amount) async {
+  void _makeFlutterwavePayment(BuildContext context, String fullname,
+      String phone, String email, String amount) async {
     try {
       Flutterwave flutterwave = Flutterwave.forUIPayment(
           //the first 10 fields below are required/mandatory
@@ -130,17 +135,15 @@ void _makeFlutterwavePayment(BuildContext context, String fullname, String phone
           acceptMpesaPayment: true,
           acceptRwandaMoneyPayment: false,
           acceptUgandaPayment: false,
-          acceptZambiaPayment: false
-          );
-   
-    final response = await flutterwave.initializeForUiPayments();
-    if (response == null) {
+          acceptZambiaPayment: false);
+
+      final response = await flutterwave.initializeForUiPayments();
+      if (response == null) {
         print("Transaction Failed");
       } else {
         if (response.status == "Transaction successful") {
           print(response.data);
           print(response.message);
-
         } else {
           print(response.message);
         }
